4569cbbcad40898843ebb9fddc29e5cc67e513c8
Ricardo Pérez <ricpelo@gmail.com>
2017-02-22 13:26:37 +0100

    Retoques en el tema y los estilos

--- conf.py
+++ conf.py
@@ -166,7 +166,9 @@ html_style = 'learning_resources.css'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'display_version': False
+}
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
