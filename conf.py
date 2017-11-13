77e3a604093292428fdebc10fb398a6cdaaef087
Ricardo Pérez <ricpelo@gmail.com>
2017-11-13 14:49:55 +0100

    Me gustan más los colores originales de Spinx

--- conf.py
+++ conf.py
@@ -106,7 +106,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'index_latex.rst', 'intr
 # show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'monokai'
+pygments_style = 'sphinx'
 
 # A list of ignored prefixes for module index sorting.
 # modindex_common_prefix = []
