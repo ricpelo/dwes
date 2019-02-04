1785d6785fd73dfbfee61dabb747f2748918428d
Ricardo Pérez <ricpelo@gmail.com>
2019-02-04 16:15:45 +0100

    Instala editor Leo en setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+# Instala requisitos de Sphinx:
 pip install -r requirements.txt
+
+# Instala extensión gitdags de LaTeX:
 mkdir -p ~/texmf/tex/latex/gitdags/
 cp gitdags.sty ~/texmf/tex/latex/gitdags/
+
+# Instala última versión de titlesec:
 unzip titlesec.zip -d ~/texmf/tex/latex
+
+# Instala editor Leo:
+sudo apt install python-pyqt5
+pip install leo
