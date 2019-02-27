180dfa7ddb0a090b399258aab5c23dd6d9fb1f05
Ricardo Pérez <ricpelo@gmail.com>
2019-02-27 13:02:43 +0100

    Instala paquetes de Python en .local

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Instala requisitos de Sphinx:
-pip install -r requirements.txt
+pip install --user -r requirements.txt
 
 # Instala extensión gitdags de LaTeX:
 mkdir -p ~/texmf/tex/latex/gitdags/
